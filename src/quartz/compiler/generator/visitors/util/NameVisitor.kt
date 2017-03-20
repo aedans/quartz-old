980283d026670120d37e2b309aa55fb755401a86
aedan <aedan.smith@gmail.com>
2017-03-19 23:07:51 -0700

    Trim escaped identifiers

--- src/quartz/compiler/generator/visitors/util/NameVisitor.kt
+++ src/quartz/compiler/generator/visitors/util/NameVisitor.kt
@@ -11,13 +11,16 @@ private val VALID_CHAR = ('a'..'z') + ('A'..'Z') +('0'..'9') + '_'
 
 val nameVisitor: Visitor<String> = { identifier, string, _ ->
     var s = ""
-    identifier.forEach {
+    (if (identifier.startsWith('`') && identifier.endsWith('`'))
+        identifier.substring(1, identifier.length-1)
+    else identifier)
+            .forEach {
         if (it in VALID_CHAR)
             s += it
         else
             s += it.toInt()
     }
-    if (s[0] !in VALID_CHAR_START) {
+    if (s.isEmpty() || s[0] !in VALID_CHAR_START) {
         s = '_' + s
     }
     string.append(s)
