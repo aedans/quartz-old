484a80567fb9a341454ec9613e9941490bebb608
aedan <aedan.smith@gmail.com>
2017-03-20 21:29:11 -0700

    Rename temp vars

--- src/quartz/compiler/semantics/translator/function/expressions/IfExpressionTranslator.kt
+++ src/quartz/compiler/semantics/translator/function/expressions/IfExpressionTranslator.kt
@@ -13,7 +13,7 @@ import quartz.compiler.syntax.tree.program.function.statement.VarDeclarationNode
  */
 
 fun IfExpressionNode.translate(newStatements: MutableList<StatementNode>): IdentifierNode {
-    val tempVarName = "_${hashCode()}"
+    val tempVarName = "temp${hashCode()}"
     newStatements.add(VarDeclarationNode(tempVarName, type ?: throw Exception("Unknown type for $this"), true, null))
     val ifStatement = IfStatementNode(test)
     ifStatement.trueStatements.add(VarAssignmentNode(tempVarName, ifTrue))
