b053c778f2dae966b45adec81116b8ce5ef2360a
aedan <aedan.smith@gmail.com>
2017-03-30 23:08:59 -0700

    Refactor cstdio

--- test/test.bat
+++ test/test.bat
@@ -6,7 +6,7 @@
 @mkdir out
 @mkdir src
 @mkdir debug
-@for %%f in (tests/*) do java -jar ../jar/Quartz.jar tests/%%f src/%%f > debug/%%f.debug
+@for %%f in (tests/*) do java -jar ../jar/Quartz.jar tests/%%f src/%%f.c > debug/%%f.debug
 @for %%f in (src/*) do nvcc src/%%f -o exe/%%f.exe --machine 32 -x cu --optimize 0 -Wno-deprecated-gpu-targets --cudart shared
 @cd exe
 @for %%f in (*.exe) do @for /F "tokens=*" %%i in ('call %%f') DO @echo %%i > ../out/%%f.txt
--- test/testc.bat
+++ test/testc.bat
@@ -6,7 +6,7 @@
 @mkdir out
 @mkdir src
 @mkdir debug
-@for %%f in (tests/*) do java -jar ../jar/Quartz.jar tests/%%f src/%%f > debug/%%f.debug
+@for %%f in (tests/*) do java -jar ../jar/Quartz.jar tests/%%f src/%%f.c > debug/%%f.debug
 @for %%f in (src/*) do gcc src/%%f -o exe/%%f.exe
 @cd exe
 @for %%f in (*.exe) do @for /F "tokens=*" %%i in ('call %%f') DO @echo %%i > ../out/%%f.txt
