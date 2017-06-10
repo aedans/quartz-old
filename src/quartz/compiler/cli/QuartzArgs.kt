7b173cd96eaee168cb3b841aa74cc470256b4f61
aedan <aedan.smith@gmail.com>
2017-06-09 17:40:27 -0700

    Add cli

--- src/quartz/compiler/cli/QuartzArgs.kt
+++ src/quartz/compiler/cli/QuartzArgs.kt
@@ -16,9 +16,9 @@ class QuartzArgs(args: Array<String>) {
     val inputFile by parser.positional("SOURCE", help = "the quartz source file") { File(this) }
     val outputFile by parser.storing("-o", "--output", help = "set the output file") { File(this) }
             .default(File("main.c"))
-    val library by parser.storing("-l", "--library", help = "sets the quartz standard library") { Library.create(File(this)) }
+    val library by parser.storing("-l", "--library", help = "set the quartz standard library") { Library.create(File(this)) }
             .default(Library.create(File(System.getenv()["QZ_LIB"] ?: throw QuartzException("Environment variable QZ_LIB is not set"))))
-    val includes by parser.adding("-I", "--include", help = "libraries to include") { Library.create(File(this)) } // TODO test
+    val includes by parser.adding("-I", "--include", help = "include libraries") { Library.create(File(this)) } // TODO test
     val time by parser.flagging("-t", "--time", help = "time the compiler in milliseconds").default(false)
     val debugBuilder by parser.flagging("--debug-builder", help = "enable debug builder").default(false)
     val debugAnalyzer by parser.flagging("--debug-analyzer", help = "enable debug analyzer").default(false)
